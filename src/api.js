b803472d7187cc73146ee7d2acb2aae3fbd79862
Nisha Yadav <ny6957880@gmail.com>
2026-06-26 16:40:36 +0530

    Fix api.js

--- src/api.js
+++ src/api.js
@@ -18,6 +18,8 @@
 //   }
 //   return `${API_BASE_URL}${path.startsWith("/") ? path : `/${path}`}`;
 // };
+
+
 import axios from "axios";
 
 export const API_BASE_URL = process.env.REACT_APP_API_URL;
