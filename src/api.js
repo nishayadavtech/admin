4c3acaf62c87d171e5319bbe5d2060d79a3bbc5f
Nisha Yadav <ny6957880@gmail.com>
2026-06-26 16:20:48 +0530

    Fixed API URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,28 @@
+// import axios from "axios";
+
+// import.meta.env.VITE_API_URL;
+
+// export const api = axios.create({
+//   baseURL: API_BASE_URL,
+// });
+
+// export const getAuthHeaders = () => {
+//   const token = localStorage.getItem("token");
+//   return token ? { Authorization: `Bearer ${token}` } : {};
+// };
+
+// export const getImageUrl = (path) => {
+//   if (!path) return "";
+//   if (path.startsWith("http://") || path.startsWith("https://")) {
+//     return path;
+//   }
+//   return `${API_BASE_URL}${path.startsWith("/") ? path : `/${path}`}`;
+// };
+
+
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5500";
+const API_BASE_URL = import.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
